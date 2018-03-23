c84048220a238bade43d546bef1d1af0e27db21b
Marko Djordjevic <makezi08@gmail.com>
2018-03-23 18:15:55 +1100

    Add header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from "./Header/"
 import MovieInfoContainer from '../containers/MovieInfoContainer';
 
 const App = () => (
   <div>
+    <Header />
     <MovieInfoContainer />
   </div>
 );
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <div className="logo">
+      <img
+        src="https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg"
+        alt="Movie DB Logo"
+      />
+    </div>
+  </header>
+);
+
+export default Header;
