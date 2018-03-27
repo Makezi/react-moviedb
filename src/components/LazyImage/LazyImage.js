7939bea6fe8c605ad017cfe5378e3aed1dc9aecb
Marko Djordjevic <makezi08@gmail.com>
2018-03-27 16:52:56 +1100

    Fix LazyImage component attempting to mount before movie data retrieved

--- src/components/LazyImage/LazyImage.js
+++ src/components/LazyImage/LazyImage.js
@@ -1,30 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import placeholder from '../../images/placeholder-poster.png';
 import './LazyImage.css';
 
-class LazyImage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loaded: false
-    };
-  }
-
-  onHandleLoad() {
-    this.setState({ loaded: true });
-  }
-
-  componentDidMount() {
-    this.img = new Image();
-    this.img.onload = this.onHandleLoad.bind(this);
-    this.img.src = this.props.src;
-  }
-
-  render() {
-    const { src, alt } = this.props;
-    const image = this.state.loaded ? src : placeholder;
-    return <img src={`${image}`} alt={`${alt}`} />;
-  }
-}
+const LazyImage = ({ src }) => {
+  const imageStyle = {
+    backgroundImage: `url(${src}), url(${placeholder})`
+  };
+  return <div className="image" style={imageStyle} />;
+};
 
 export default LazyImage;
