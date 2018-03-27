cc4bf3e446a61b72331d5049077d48adec41ffd3
Marko Djordjevic <makezi08@gmail.com>
2018-03-27 19:21:26 +1100

    Add links from discover list to specific movie page

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Poster from '../Poster';
 import './MovieCard.css';
 
 const MovieCard = ({ movie }) => (
   <div className="movie-card">
     <Poster img={movie.poster_path}>
-      <span className="title">{movie.original_title}</span>
+      <Link to={`/movie/${movie.id}`}>
+        <span className="title">{movie.original_title}</span>
+      </Link>
     </Poster>
   </div>
 );
