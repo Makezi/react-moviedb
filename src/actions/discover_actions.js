b76d144c341e84fca615aa3c8f54c7dae9aebec5
Marko Djordjevic <makezi08@gmail.com>
2018-03-28 17:20:46 +1100

    Refactor fetching data from actions into separate function

--- src/actions/discover_actions.js
+++ src/actions/discover_actions.js
@@ -1,46 +1,21 @@
-import axios from 'axios';
-import { BASE_API_URL, API_KEY, API_CACHE_DURATION } from '../constants/api';
+import { isDataStale, fetchData } from './action_helpers';
+import { BASE_API_URL, API_KEY } from '../constants/api';
 import {
   FETCH_DISCOVER_MOVIES,
   FETCH_DISCOVER_MOVIES_IS_LOADING,
   FETCH_DISCOVER_MOVIES_HAS_ERRORED
 } from '../constants/action_types';
 
-function isDataStale(data) {
-  let lastFetched = 0;
-  if (data && data.lastFetched !== undefined) {
-    lastFetched = data.lastFetched;
-  }
-  return Date.now() - lastFetched > lastFetched + API_CACHE_DURATION;
-}
-
-function discoverMoviesIsLoading(bool) {
-  return {
-    type: FETCH_DISCOVER_MOVIES_IS_LOADING,
-    payload: bool
-  };
-}
-
-function discoverMoviesHasErrored(bool) {
-  return {
-    type: FETCH_DISCOVER_MOVIES_HAS_ERRORED,
-    payload: bool
-  };
-}
-
 export function fetchDiscoverMovies(page = 1) {
   const url = `${BASE_API_URL}discover/movie?api_key=${API_KEY}&page=${page}`;
   return (dispatch, getState) => {
     if (!isDataStale(getState().discover.pages[page])) return;
-    dispatch(discoverMoviesIsLoading(true));
-    axios
-      .get(url)
-      .then(response =>
-        dispatch({ type: FETCH_DISCOVER_MOVIES, payload: response.data })
-      )
-      .then(() => {
-        dispatch(discoverMoviesIsLoading(false));
-      })
-      .catch(() => dispatch(discoverMoviesHasErrored(true)));
+    fetchData(
+      dispatch,
+      url,
+      FETCH_DISCOVER_MOVIES,
+      FETCH_DISCOVER_MOVIES_IS_LOADING,
+      FETCH_DISCOVER_MOVIES_HAS_ERRORED
+    );
   };
 }
--- src/actions/movies_actions.js
+++ src/actions/movies_actions.js
@@ -1,45 +1,22 @@
-import axios from 'axios';
-import { BASE_API_URL, API_KEY, API_CACHE_DURATION } from '../constants/api';
+import { isDataStale, fetchData } from './action_helpers';
+import { BASE_API_URL, API_KEY } from '../constants/api';
 import {
   FETCH_MOVIE,
   FETCH_MOVIE_IS_LOADING,
   FETCH_MOVIE_HAS_ERRORED
 } from '../constants/action_types';
 
-function isDataStale(data) {
-  let lastFetched = 0;
-  if (data && data.lastFetched !== undefined) {
-    lastFetched = data.lastFetched;
-  }
-  return Date.now() - lastFetched > lastFetched + API_CACHE_DURATION;
-}
-
-function fetchMovieIsLoading(bool) {
-  return {
-    type: FETCH_MOVIE_IS_LOADING,
-    payload: bool
-  };
-}
-
-function fetchMovieHasErrored(bool) {
-  return {
-    type: FETCH_MOVIE_HAS_ERRORED,
-    payload: bool
-  };
-}
-
 export function fetchMovie(id) {
   const url = `${BASE_API_URL}movie/${id}?api_key=${API_KEY}`;
   return (dispatch, getState) => {
     const movie = getState().movies.byId[id];
     if (!isDataStale(movie)) return;
-    dispatch(fetchMovieIsLoading(true));
-    axios
-      .get(url)
-      .then(response => dispatch({ type: FETCH_MOVIE, payload: response.data }))
-      .then(() => {
-        dispatch(fetchMovieIsLoading(false));
-      })
-      .catch(() => dispatch(fetchMovieHasErrored(true)));
+    fetchData(
+      dispatch,
+      url,
+      FETCH_MOVIE,
+      FETCH_MOVIE_IS_LOADING,
+      FETCH_MOVIE_HAS_ERRORED
+    );
   };
 }
