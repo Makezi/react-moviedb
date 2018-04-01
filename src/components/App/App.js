b1ebf36a2dd9afa203cb770313e18600c7cff690
Marko Djordjevic <makezi08@gmail.com>
2018-04-01 19:37:48 +1000

    Add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Header from "../Header/";
-import Main from "../Main/";
-import styles from "./App.scss";
+import Header from '../Header/';
+import Main from '../Main/';
+import Footer from '../Footer';
+import styles from './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <div className={styles.wrapper}>
       <Header />
       <Main />
+      <Footer />
     </div>
   </BrowserRouter>
 );
