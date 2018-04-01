363fbcbb8c52f79bfe1e605a7b4d196c841fae32
Marko Djordjevic <makezi08@gmail.com>
2018-04-01 20:56:01 +1000

    Fix header not routing back to home page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,7 @@ import styles from './Main.scss';
 const Main = () => (
   <main className={styles.main}>
     <Switch>
-      <Route path="/react-moviedb" exact component={DiscoverPage} />
+      <Route path="/react-moviedb/" exact component={DiscoverPage} />
       <Route path="/react-moviedb/discover/:page" component={DiscoverPage} />
       <Route path="/react-moviedb/movie/:id" component={MoviePage} />
       <Route path="/react-moviedb/404" component={NotFoundPage} />
