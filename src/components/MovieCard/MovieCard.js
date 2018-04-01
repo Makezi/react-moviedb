75cc267748c243ec34f4831b87acb7ed189132f9
Marko Djordjevic <makezi08@gmail.com>
2018-04-01 19:20:25 +1000

    Add keyboard controls for movie cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,21 +5,18 @@ import moment from 'moment';
 import styles from './MovieCard.scss';
 
 const MovieCard = ({ movie }) => (
-  <div className={styles.card}>
-    <Link to={`/movie/${movie.id}`}>
-      <Poster img={movie.poster_path} title={movie.original_title} />
-      <div className={styles.details}>
-        {movie.vote_average ? (
-          <span className={styles.voteAverage}>{movie.vote_average * 10}%</span>
-        ) : null}
-        <span className={styles.title}>
-          {`${movie.title} (${moment(
-            movie.release_date
-          ).format('YYYY') || null})`}
-        </span>
-      </div>
-    </Link>
-  </div>
+  <Link className={styles.card} to={`/movie/${movie.id}`}>
+    <Poster img={movie.poster_path} title={movie.original_title} />
+    <div className={styles.details}>
+      {movie.vote_average ? (
+        <span className={styles.voteAverage}>{movie.vote_average * 10}%</span>
+      ) : null}
+      <span className={styles.title}>
+        {`${movie.title} (${moment(movie.release_date).format('YYYY') ||
+          null})`}
+      </span>
+    </div>
+  </Link>
 );
 
 export default MovieCard;
